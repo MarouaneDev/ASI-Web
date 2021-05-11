25bfd6e3e3e89ba5991c6580ec32904bd7f86d0b
nassimk69 <nassim.khirredine@gmail.com>
2021-05-11 15:57:04 +0200

    add routes & delete useless classes

--- src/main/java/com/sp/rest/CarteRestCrt.java
+++ src/main/java/com/sp/rest/CarteRestCrt.java
@@ -8,13 +8,18 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sp.model.Carte;
+import com.sp.model.User;
 import com.sp.service.CarteService;
+import com.sp.service.UserService;
 
 @RestController
 public class CarteRestCrt {
 	
 	@Autowired
 	CarteService cService;
+	
+	@Autowired
+	UserService uService;
 	
 	@RequestMapping(method=RequestMethod.POST,value="/carte")
     public void addCarte(@RequestBody Carte carte) {
@@ -25,6 +30,14 @@ public class CarteRestCrt {
     public Carte getCarte(@PathVariable String id) {
         Carte c=cService.getCarte(Integer.valueOf(id));
         return c;
+    }
+	
+	@RequestMapping(method=RequestMethod.POST,value="/buy/carte/{cardId}")
+    public void buyCard(@PathVariable String cardId, String userId) {
+        Carte c = cService.getCarte(Integer.valueOf(cardId));
+        User u = uService.getUser(Integer.valueOf(userId));
+		cService.buyCard(c, u);
+		
     }
 
 
