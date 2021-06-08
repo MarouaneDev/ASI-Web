5dbba401532e47744ddef0b41bb0b570994f642f
Emilio-mld <em69002@gmail.com>
2021-06-08 10:15:12 +0200

    finished tests for CardService

--- CardServicee/src/test/java/com/asi/service/card/CardService/CardServiceApplicationTests.java
+++ CardServicee/src/test/java/com/asi/service/card/CardService/CardServiceApplicationTests.java
@@ -1,19 +1,19 @@
 package com.asi.service.card.CardService;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import com.asi.service.card.CardService.model.Carte;
 import com.asi.service.card.CardService.service.CarteService;
+import com.asi.service.card.CardService.service.UserService;
 import com.asi.service.card.CardService.model.User;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class CardServiceApplicationTests {
 
 	Carte card = new Carte(1, "Darxul", "Marche avec les mains", ":-)", "Nwar", "Pieds", 100,
@@ -30,6 +30,9 @@ class CardServiceApplicationTests {
 	@Autowired
 	CarteService cardService = new CarteService();
 
+	@Autowired
+	UserService userService = new UserService();
+
 	@Test
 	@Order(1)
 	void add_get_CardTest() {
@@ -45,17 +48,13 @@ class CardServiceApplicationTests {
 
 	@Test
 	@Order(3)
-	void buyCardTest(){
+	void buy_sell_CardTest(){
 		user.increaseWallet(20000);
 		cardService.buyCard(card, user);
 		assertEquals(card.getName(), user.getOwnedCards().get(0).getName());
-	}
-
-	@Test
-	@Order(4)
-	void sellCardTest(){
 		long wallet = user.getWalletAmount();
-		CarteService.sellCard(user.getOwnedCard().get(0), user);
+		cardService.sellCard(user.getOwnedCards().get(0), user);
 		assertNotEquals(wallet, user.getWalletAmount());
 	}
+
 }
