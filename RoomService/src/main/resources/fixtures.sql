e6213a856f13b50c6c6b8e161eff1ea32d9b604b
nassimk69 <nassim.khirredine@gmail.com>
2021-06-08 09:28:21 +0200

    add fixtures for : card, user & room

---
+++ RoomService/src/main/resources/fixtures.sql
@@ -0,0 +1,3 @@
+INSERT INTO Room (id, room_name) values ('1', 'Red Room');
+INSERT INTO Room (id, room_name) values ('2', 'Daryl Room');
+INSERT INTO Room (id, room_name) values ('3', 'OkayMany R00M');
